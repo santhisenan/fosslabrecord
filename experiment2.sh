01128ae9a2637403f26d199fb54da6e08e828108
santhisenan <santhisenan@outlook.com>
2017-02-25 14:54:54 +0530

    experiment 2 done

--- experiment2.sh
+++ experiment2.sh
@@ -1,3 +1,20 @@
+:'Experiment 2
+SHELL PROGRAMMING
+DATE : 17-02-2017
+AIM
+
+Write shell script to show various system configuration like
+1.Currently logged user and his logname
+2.Your current shell
+3.Your home directory
+4.Your operating system type
+5.Your current path setting
+6.Your current working directory
+7.Show Currently logged number of users
+
+'
+
+
 clear
 log=`who|wc -l`
 echo "the currently logged in user is $USER"
