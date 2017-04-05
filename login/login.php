96aeccb1be7431adaa237dad25c71356a2516f5b
santhisenan <santhisenan@outlook.com>
2017-04-05 05:55:06 +0530

    login update

--- login/login.php
+++ login/login.php
@@ -1,9 +1,5 @@
 <?php 
-define('DB_SERVER','localhost');
-define('DB_USERNAME','fc');
-define('DB_PASSWORD','');
-define('DB_DATABASE','fossdb');
-$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+include("config.php");
 $error="";
 if($_SERVER["REQUEST_METHOD"] == "POST"){
 	$myusername = mysqli_real_escape_string($db,$_POST['username']);
@@ -13,11 +9,11 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
       $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
       $active = $row['active'];
       $count = mysqli_num_rows($result);
+
       if($count == 1) {
          header("location: welcome.php");
       }else {
-        echo $result;
-        $error = "Your Login Name or Password is invalid";
+         $error = "Your Login Name or Password is invalid";
       }
 }
 
@@ -31,7 +27,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 
 <body>
 <div><?php echo $error ?></div>
-	<form action="login.php" method="POST">
+	<form action="" method="POST">
 		<input type="text" placeholder="username" name="username">
 		<input type="password" placeholder="password" name="password">
 		<input type="submit" value="submit">
